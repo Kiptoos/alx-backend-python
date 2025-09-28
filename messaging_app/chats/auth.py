80c1cc3c7fbfb48589daf917655f6682f74852b6
KiptooStephen <kiptoos@gmail.com>
2025-09-28 23:35:30 +0300

    Update auth.py

--- messaging_app/chats/auth.py
+++ messaging_app/chats/auth.py
@@ -1,1 +1,14 @@
 # Custom auth helpers or token serializers can live here
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["username"] = user.get_username()
+        token["is_staff"] = user.is_staff
+        return token
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
