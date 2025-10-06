4bc001ad65d9fa0968b54968c228064e0faa67d6
KiptooStephen <kiptoos@gmail.com>
2025-10-06 22:56:21 +0300

    Update url.py

--- Django-signals_orm-0x04/messaging_app/url.py
+++ Django-signals_orm-0x04/messaging_app/url.py
@@ -1,3 +1,5 @@
 # project urls.py (e.g., messaging_app/messaging_app/urls.py)
 from django.urls import path, include
-urlpatterns = [ path("chats/", include("chats.urls")) ]
+urlpatterns = [ path("chats/", include("chats.urls")) 
+              path("messages/", include("messaging.urls")),
+              ]
