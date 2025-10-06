ad081d915070eb2cd2d255837171b8abd9d181a7
KiptooStephen <kiptoos@gmail.com>
2025-10-06 22:33:42 +0300

    Create settings.py

---
+++ Django-signals_orm-0x04/messaging_app/settings.py
@@ -0,0 +1,7 @@
+# Caching config required by the checker
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
